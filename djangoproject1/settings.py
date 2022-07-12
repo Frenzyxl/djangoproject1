7d6d67b9627c0aa9ab5ab8eb90aa9e95bb1279c0
Excel <oexcel77@gmail.com>
2022-07-12 17:04:06 +0000

    Fix environment variables

--- djangoproject1/settings.py
+++ djangoproject1/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q(ao#e7%xdlx6gs_i^6*zxt5fax99*(t9p(89m_ny*^10k0@ox'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
