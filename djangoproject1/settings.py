4200f7178e89a939e7fb64a12b441b6c8805f24e
Excel <oexcel77@gmail.com>
2022-07-13 14:41:21 +0000

    Fix secret key

--- djangoproject1/settings.py
+++ djangoproject1/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = 'django-insecure-q(ao#e7%xdlx6gs_i^6*zxt5fax99*(t9p(89m_ny*^10k0@ox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
